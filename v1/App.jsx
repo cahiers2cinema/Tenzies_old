98e88a77083b078f95dab9475cb4d72647ae6f5a
cahiers2cinema <161984526+cahiers2cinema@users.noreply.github.com>
2024-05-11 17:43:24 -0700

    Initial commit

---
+++ v1/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function App () {
+    return (
+    <div> 
+    <main>
+    <h1>Howdy Funks</h1>
+    </main>
+    </div>
+    )
+    
+}
---
+++ v2/components/Die.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+export default function Die(props) {
+    return (
+        <div className="die--container">
+        <div className="die">
+        <p>1</p>
+        </div>
+        <div className="die">
+        <p>2</p>
+        </div>
+        <div className="die">
+        <p>3</p>
+        </div>
+        <div className="die">
+        <p>4</p>
+        </div>
+        <div className="die">
+        <p>5</p>
+        </div>
+        <div className="die">
+        <p>6</p>
+        </div>
+        <div className="die">
+        <p>1</p>
+        </div>
+        <div className="die">
+        <p>2</p>
+        </div>
+        <div className="die">
+        <p>3</p>
+        </div>
+        <div className="die">
+        <p>4</p>
+        </div>
+        </div>
+    )
+}
